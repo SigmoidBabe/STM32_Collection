ca83b7b3643bdfe6ba822705a0f11e65e4d96b45
SigmoidBabe <99307891+SigmoidBabe@users.noreply.github.com>
2024-01-08 22:33:41 +0700

    Add 12C communication for CMPS14 IMU Sensor
    CMPS14 imu code communicating with STM32 F401RE using i2c protocol

---
+++ Coba_CMPS14/coba_cmps14.cpp
@@ -0,0 +1,42 @@
+#include "mbed.h"
+
+#define i2c_address 0x60
+
+int BEARING_Register;
+char bits[2];
+int _byteHigh;
+int _byteLow;
+int bearing;
+
+I2C CMPS(PB_4, PA_8);
+Serial pc(USBTX, USBRX);
+
+int main()
+{
+    BEARING_Register = 0x02;
+    pc.baud(9600);
+    while(1)
+    {
+        CMPS.unlock();
+        CMPS.start();
+    // to indicate an i2c read, shift the 7 bit address up 1 bit and set bit 0 to a 1
+        CMPS.write(i2c_address << 1); 
+        int writeResult = CMPS.write(BEARING_Register);
+        if(writeResult != 1)
+        {
+            pc.printf("%d\n", writeResult);
+            return 0;
+        }
+        else
+        {
+            CMPS.stop();
+            CMPS.read(i2c_address <<1, bits, 2);
+            _byteHigh = bits[0];
+            _byteLow = bits[1];
+            bearing = ((_byteHigh<<=8) + _byteLow) / 10;
+            pc.printf("Bearing : %d\n", bearing);
+        }
+    }
+}
+
+ 
